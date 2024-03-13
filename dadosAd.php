eb429238887322f4a3fe84ba18085b4797d73a87
Carlos <carlos.usjt@hotmail.com>
2024-03-13 11:29:09 -0300

    sistema de gestão única

--- dadosAd.php
+++ dadosAd.php
@@ -1,12 +1,12 @@
 <?php require_once("templates/header.php"); ?>
 
-    <?php  echo 'ID do USUÁRIO no banco: ' . $_SESSION['userid']; ?>
-     <?php echo '<br>'; ?>
-    <div style="font-size: 12px; color: green; font-weight: bold; text-align: left" id="oi">
-      <?php
+   
+    <div style="margin-top: 35px; margin-left: 35px; background-color: brown; font-size: 12px; color: white; font-weight: bold; text-align: left" id="oi">
+    
+    <?php
       // echo '<hr>';
-
-     
+      echo 'ID do USUÁRIO no banco: ' . $_SESSION['userid']; 
+      echo '<br>'; 
       echo 'Dados do AD:';
       echo '<br>';
       echo 'Login - ' . $_SESSION['login'];
@@ -25,5 +25,7 @@
       echo '<br>';
       echo 'Dados do user: ' . $_SESSION['uid'];
       echo '<br>';
-      echo 'token para autenticar em outros sistemas: ' . $_SESSION['auth_token']
-      ?>
+      echo 'token para autenticar em outros sistemas: ' . $_SESSION['auth_token'];
+    
+       ?>
+      
