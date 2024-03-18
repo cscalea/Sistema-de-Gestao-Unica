cfd137629cfc5cbc2df74213cc7bac659efe19d9
Carlos <carlos.usjt@hotmail.com>
2024-03-18 14:19:39 -0300

    fs

--- allUsersFromAd.php
+++ allUsersFromAd.php
@@ -1,91 +1,21 @@
 <style>
-  body {
-    font-family: 'lato', sans-serif;
-  }
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
 
-  .container {
-    max-width: 1000px;
-    margin-left: auto;
-    margin-right: auto;
-    padding-left: 10px;
-    padding-right: 10px;
-  }
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 2px;
+}
 
-  h2 {
-    font-size: 26px;
-    margin: 20px 0;
-    text-align: center;
-
-    small {
-      font-size: 0.5em;
-    }
-  }
-
-  .responsive-table {
-    li {
-      border-radius: 3px;
-      padding: 5px 10px;
-      display: flex;
-      justify-content: space-between;
-      margin-bottom: 5px;
-    }
-
-    .table-header {
-      background-color: #95A5A6;
-      font-size: 14px;
-      text-transform: uppercase;
-      letter-spacing: 0.02em;
-    }
-
-    /* .table-row {
-    background-color: #ffffff;
-    box-shadow: 0px 0px 9px 0px rgba(0,0,0,0.1);
-  } */
-    .col-1 {
-      flex-basis: 40%;
-    }
-
-    .col-2 {
-      flex-basis: 20%;
-    }
-
-    .col-3 {
-      flex-basis: 35%;
-    }
-
-
-    @media all and (max-width: 767px) {
-      .table-header {
-        display: none;
-      }
-
-      .table-row {}
-
-      li {
-        display: inline-block;
-      }
-
-      .col {
-
-        flex-basis: 100%;
-
-      }
-
-      .col {
-        display: flex;
-        padding: 10px 0;
-
-        &:before {
-          color: #6C7A89;
-          padding-right: 10px;
-          content: attr(data-label);
-          flex-basis: 50%;
-          text-align: right;
-        }
-      }
-    }
-  }
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
 </style>
+
 <?php
 $ldap_server = "10.15.16.191";
 $dominio = "ipem.sp"; //Dominio local ou global
@@ -106,46 +36,42 @@ if ($ldapcon) {
   if ($ldapbind) { //SUCESSO NA CONEXÃO
     // $this->verifyUser($_SESSION['login']); //FIRST ACCESS ???
     $filter = "(objectClass=user)"; //CONFIGURAÇÃO PARA RESGATAR DADOS DO USUÁRIO LOGADO
-    $attributes = array("cn", "samaccountname", "mail");
+    $attributes = array("cn", "samaccountname", "mail", "department");
     $search_result = ldap_search($ldapcon, $ldap_base_dn, $filter, $attributes);
     $entry = ldap_first_entry($ldapcon, $search_result);
-    $fullname = ldap_get_values($ldapcon, $entry, "cn")[0];
-    $email = ldap_get_values($ldapcon, $entry, "mail")[0];
-    $name = ldap_get_values($ldapcon, $entry, "samaccountname")[0];
+    // $fullname = ldap_get_values($ldapcon, $entry, "cn")[0];
+    // $email = ldap_get_values($ldapcon, $entry, "mail")[0];
+    // $name = ldap_get_values($ldapcon, $entry, "samaccountname")[0];
+    // $departamento = ldap_get_values($ldapcon, $entry, "samaccountname")[0];
     $ldap_entries = ldap_get_entries($ldapcon, $search_result);
 ?>
 
     <a href="index.php" ;>home</a>
-    <ul class="responsive-table">
-      <li class="table-header">
-        <div class="col col-1">Nome</div>
-        <div class="col col-2">Login</div>
-        <div class="col col-3">E-mail</div>
-      </li>
-      <?php foreach ($ldap_entries as $entry) : { ?>
-          <li class="table-row">
-            <div class="col col-1" data-label="Job Id">
-              <?php if (isset($entry["cn"][0])) {
+    <table>
+  <tr>
+    <th>Nome</th>
+    <th>Login</th>
+    <th>E-mail</th>
+    <th>Setor</th>
+  </tr>
+  
+  <?php foreach ($ldap_entries as $entry) : { ?>
+  <tr>
+    <td><?php if (isset($entry["cn"][0])) {
                       echo "Nome completo: " . $entry["cn"][0] . "<br>";
-                    }
-              ?>
-            </div>
-            <div class="col col-2" data-label="Customer Name">
-              <?php if (isset($entry["samaccountname"][0])) {
+                    } ?></td>
+    <td><?php if (isset($entry["samaccountname"][0])) {
                         echo "Login: " . $entry["samaccountname"][0] . "<br>";
-                    }
-              ?>
-            </div>
-            <div class="col col-3" data-label="Amount">
-              <?php if (isset($entry["mail"][0])) {
+                    } ?></td>
+    <td><?php if (isset($entry["mail"][0])) {
                         echo "E-mail: " . $entry["mail"][0] . "<br>";
-                    }
-              ?>
-              </div>
-          </li>
-      <?php }
-      endforeach; ?>
-    </ul>
-<?php
+                    } ?></td>
+    <td><?php if (isset($entry["department"][0])) {
+                        echo "Departamento: " . $entry["department"][0] . "<br>";
+                    } ?> </td>
+  </tr>
+  <?php } endforeach; ?>
+</table>
+  <?php
   }
 }
